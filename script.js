aaf295395063b6d18b56b9eb4e8578fa994aa399
Silver Nhan <votrinhnhan1403@gmail.com>
2022-11-07 17:26:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,6 @@
 var is_mobile = !!navigator.userAgent.match(/iphone|android|blackberry/ig) || false;
 $(document).ready(function() {
     GL = document.getElementById("glcanvas");
-    if (is_mobile) {
-        GL.setAttribute("class", "glcanvas_moblie");
-        console.log("on moblie")
-    } else {console.log("on pc ")}
     picture_width = GL.offsetWidth;
     picture_height = GL.offsetHeight;
     document.getElementById("VARJO").setAttribute("width", picture_width);
