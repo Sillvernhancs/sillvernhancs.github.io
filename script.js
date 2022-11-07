dd4513cf0136912bdab23103f54a9ef2ef8d3362
Silver Nhan <votrinhnhan1403@gmail.com>
2022-11-07 13:34:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+
+var is_mobile = !!navigator.userAgent.match(/iphone|android|blackberry/ig) || false;
 $(document).ready(function() {
     GL = document.getElementById("glcanvas");
+    if (is_mobile) {
+        GL.setAttribute("width", "100%");
+        GL.setAttribute("height", "100%");
+    }
     picture_width = GL.offsetWidth;
     picture_height = GL.offsetHeight;
     document.getElementById("VARJO").setAttribute("width", picture_width);
@@ -7,6 +13,7 @@ $(document).ready(function() {
     document.getElementById("block").setAttribute("width", picture_width);
 
     document.getElementById("secondBackground").play();
+
 });
 
 window.addEventListener('resize', function(event) {
