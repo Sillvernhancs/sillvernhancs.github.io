4bdfaf9c659e74aa1ce72424579a327ba8991d37
Silver Nhan <votrinhnhan1403@gmail.com>
2022-11-17 13:09:09 -0600

    added one more page of content

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ $(document).ready(function() {
     document.getElementById("thirdBackground").setAttribute("width", picture_width + 50);
     document.getElementById("block").setAttribute("width", picture_width);
     
-    document.getElementById("source_img").setAttribute("heigh", picture_height);
     document.getElementById("source_img").setAttribute("width", picture_width);
+
+    document.getElementById("video").setAttribute("height", picture_height/1.5 );
+    document.getElementById("video").setAttribute("width", picture_width/1.5);
+
+    document.getElementById("video_page").setAttribute("height", picture_height);
+    document.getElementById("video_page").setAttribute("width", picture_width);
 
     document.getElementById("secondBackground").play();
     document.getElementById("secondBackground").play();
@@ -25,7 +30,9 @@ window.addEventListener('resize', function(event) {
     document.getElementById("thirdBackground").setAttribute("width", picture_width + 50);
 
     document.getElementById("source_img").setAttribute("width", picture_width);
-    document.getElementById("source_img").setAttribute("heigh", picture_height);
+
+    document.getElementById("video_page").setAttribute("height", picture_height);
+    document.getElementById("video_page").setAttribute("width", picture_width);
 
     document.getElementById("block").setAttribute("width", picture_width);
 
